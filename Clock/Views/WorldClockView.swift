92a6ad810136647ea578c297b1ae7cba854e4784
Ethan Shi <eshi@lcs.on.ca>
2026-02-24 14:43:38 -0500

    Finished one row of the World Clock time zone interface.

--- Clock/Views/WorldClockView.swift
+++ Clock/Views/WorldClockView.swift
@@ -10,27 +10,46 @@ import SwiftUI
 struct WorldClockView: View {
     var body: some View {
         NavigationStack {
-            Text("World Clock")
-                .navigationTitle("World Clock")
-                .toolbar {
-                    
-                    ToolbarItem(placement: .topBarLeading) {
-                        
-                        Button("Edit") {
-                            // Does nothing right now
-                        }
-                        
+            VStack{
+                HStack{
+                    VStack{
+                        Text("Today, +0HRS")
+                            .font(.system(size: 15))
+                        Text("Ottawa")
+                            .font(.system(size: 30))
                     }
-                    ToolbarItem(placement: .primaryAction) {
-                        Button {
-                        } label: {
-                            Image(systemName: "plus")
-                        }
+                    Spacer()
+                    Text("6:35")
+                        .font(.system(size: 50))
+                    Text("AM")
+                        .font(.system(size: 30))
+                }
+            }
+            .navigationTitle("World Clock")
+            .toolbar {
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    
+                    Button("Edit") {
+                        // Does nothing right now
+                    }
+                    
+                }
+                ToolbarItem(placement: .primaryAction) {
+                    Button {
+                    } label: {
+                        Image(systemName: "plus")
                     }
                 }
-
+            }
+            
         }
+        
+        
+        
+        
     }
+    
 }
 #Preview {
     LandingView()
