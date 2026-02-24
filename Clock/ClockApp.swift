dcc2373bc6a8f0b933650fe049faa4f6ed0d7e0e
Ethan Shi <eshi@lcs.on.ca>
2026-02-24 14:12:39 -0500

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Yicheng Shi on 2026-02-24.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
